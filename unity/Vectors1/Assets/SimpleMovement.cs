38380a8e70f6b7b5eb024cecfd620086b9b5d8a6
Bryan Duggan <skooter500@gmail.com>
2015-09-30 08:59:19 +0100

    Added squaremovement code

---
+++ unity/Vectors1/Assets/SimpleMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float speed = 10.0f;
+        float aSpeed = 90.0f;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+            
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, 0, - speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up, - aSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up, aSpeed * Time.deltaTime);
+        }
+
+
+    }
+}
---
+++ unity/Vectors1/Assets/SquareMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquareMovement : MonoBehaviour {
+
+    public float distance = 20;
+    float travelled;
+	// Use this for initialization
+	void Start ()
+    {
+        travelled = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float speed = 20.0f;
+        float off = speed * Time.deltaTime;
+        travelled += off;    
+        if (travelled > distance)
+        {
+            travelled = 0.0f;
+            transform.Rotate(Vector3.up, 90.0f);
+        }
+        transform.Translate(0, 0, off);
+    }
+}
