d9b35ab611d07cafd64e36444682daa5fb325de0
Bryan Duggan <skooter500@gmail.com>
2015-10-20 17:55:29 +0100

    Bla

---
+++ unity/Vectors1/Assets/CirclePath.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(PathFollower))]
+public class CirclePath : MonoBehaviour {
+    public float radius = 50;
+    public float thetaOffset = 0.0f;
+    public int numberOfWaypoints = 12;
+    public float height;
+    private PathFollower pathFollower;    
+
+    // Use this for initialization
+    void Start () {
+
+        pathFollower = GetComponent<PathFollower>();
+
+        float thetaInc = Mathf.PI * 2.0f / (float)numberOfWaypoints;
+        for (int i = 0; i < numberOfWaypoints; i++)
+        {
+            float theta = thetaOffset + (thetaInc * i);
+            Vector3 pos = new Vector3();
+            pos.x = transform.position.x + (Mathf.Sin(theta) * radius);
+            pos.z = transform.position.z + (Mathf.Cos(theta) * radius);
+            pathFollower.waypoints.Add(pos);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ unity/Vectors1/Assets/SphereParticle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SphereParticle:MonoBehaviour
+{
+    public Vector3 velocity = Vector3.zero;
+    public Vector3 gravity = new Vector3(0, -9, 0);
+    public float alive = 0.0f;
+
+    public void Update()
+    {
+        velocity += gravity * Time.deltaTime;
+        transform.Translate(velocity * Time.deltaTime, Space.World);
+        if (transform.position.y < 0.5f)
+        {
+            velocity.y = -velocity.y;
+            Vector3 pos = transform.position;
+            pos.y = 0.5f;
+            transform.position = pos;
+            alive += Time.deltaTime;
+        }
+    }
+}
